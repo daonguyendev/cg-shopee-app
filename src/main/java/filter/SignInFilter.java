503203289b70eb26e2451f787ec9df84c4a37ba6
LamChiHung <146713547+LamChiHung@users.noreply.github.com>
2024-01-06 21:13:09 +0700

    Create SignIn Filter

---
+++ src/main/java/filter/SignInFilter.java
@@ -0,0 +1,39 @@
+package filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter (urlPatterns = {"/cart","/add","/delete","/payment","/product","/user"})
+public class SignInFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        if (httpServletRequest.getSession().getAttribute("user")==null)
+        {
+            httpServletResponse.sendRedirect("/signin-signup");
+        }
+        else {
+            chain.doFilter(httpServletRequest,httpServletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
