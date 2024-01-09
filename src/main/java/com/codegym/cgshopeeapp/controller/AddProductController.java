2e8eb1df281bcfc3213b170bf41f4a3ee66c016c
LamChiHung <146713547+LamChiHung@users.noreply.github.com>
2024-01-09 16:45:40 +0700

    Modify Add product to cart feature

--- src/main/java/com/codegym/cgshopeeapp/controller/AddProductController.java
+++ src/main/java/com/codegym/cgshopeeapp/controller/AddProductController.java
@@ -23,16 +23,28 @@ public class AddProductController extends HttpServlet {
         HttpSession httpSession = request.getSession();
         Cart cart = (Cart) httpSession.getAttribute("cart");
         int id = Integer.parseInt(request.getParameter("id"));
-        Product product = ProductDao.getById(id);
-        cart.getProductArrayList().add(product);
+        boolean isValid = true;
+        for (Product item : cart.getProductArrayList()){
+            if (item.getId() == id){
+                isValid = false;
+            }
+        }
         RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/home");
-        message = "Đã thêm sản phẩm thành công";
+        if (isValid) {
+            Product product = ProductDao.getById(id);
+            cart.getProductArrayList().add(product);
+            message = "Đã thêm sản phẩm thành công";
+        }
+        else {
+            message = "Sản phẩm đã có trong giỏ hàng";
+        }
         request.setAttribute("message",message);
         try {
             requestDispatcher.forward(request,response);
         } catch (ServletException e) {
             throw new RuntimeException(e);
         }
+
     }
     public void destroy() {
     }
